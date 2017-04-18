a006c81a6fdaccf852f033ec6c4d3accbe3618c1
Megan Reiffer <meganreiffer@gmail.com>
2017-04-17 21:55:18 -0400

    Initial commit

---
+++ app/src/main/java/mine/com/meagain/locationbasedservices/MainActivity.java
@@ -0,0 +1,102 @@
+package mine.com.meagain.locationbasedservices;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+    LocationListener mLocListener;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("demo","resume");
+        if (!locationManager.isProviderEnabled(locationManager.GPS_PROVIDER)) {
+            Log.d("demo","location disabled");
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("GPS Not Enabled")
+                    .setMessage("Would you like to enable GPS settings? ")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            startActivity(intent);
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                            finish();
+                        }
+                    });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        } else {
+            Log.d("demo","location enabled");
+            mLocListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    Log.d("demo", "onLocationChanged: " + location.getLatitude() + ", " + location.getLongitude());
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            };
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                Log.d("demo: ", "Location off");
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        8);
+
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 0, 10, mLocListener);
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+
+
+    }
+}
